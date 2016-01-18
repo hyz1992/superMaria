3b43737c5b4d08a9a6b8b46454e86836519b35ca
hyz1992 <18086089679@163.com>
2016-01-19 01:21:14 +0800

    使砖块被撞碎

--- workSpace/src/app/entity/brick.lua
+++ workSpace/src/app/entity/brick.lua
@@ -16,7 +16,34 @@ end
 
 function brick:goDead()
 	self:clearSelf()
-	
+	self._spr:hide()
+	local texeture2D = self._spr:getSpriteFrame():getTexture()
+	local size = self._spr:getContentSize()
+	local rect = self._spr:getSpriteFrame():getRect()
+	printTable(size)
+	printTable(rect)
+	local sp_1 = cc.Sprite:createWithTexture(texeture2D,cc.rect(rect.x,rect.y,size.width/2,size.height/2))
+			:addTo(self)
+			:move(cc.p(-size.width*0.25,size.height*0.75))
+	local sp_2 = cc.Sprite:createWithTexture(texeture2D,cc.rect(rect.x+size.width/2,rect.y,size.width/2,size.height/2))
+			:addTo(self)
+			:move(cc.p(size.width*0.25,size.height*0.75))
+	local sp_3 = cc.Sprite:createWithTexture(texeture2D,cc.rect(rect.x,rect.y+size.height/2,size.width/2,size.height/2))
+			:addTo(self)
+			:move(cc.p(-size.width*0.25,size.height*0.25))
+	local sp_4 = cc.Sprite:createWithTexture(texeture2D,cc.rect(rect.x+size.width/2,rect.y+size.height/2,size.width/2,size.height/2))
+			:addTo(self)
+			:move(cc.p(size.width*0.25,size.height*0.25))
+
+	local moveTime = 0.2
+	local distance = 40
+	sp_1:runAction(cc.MoveBy:create(moveTime,cc.p(-distance,distance)))
+	sp_2:runAction(cc.MoveBy:create(moveTime,cc.p(distance,distance)))
+	sp_3:runAction(cc.MoveBy:create(moveTime,cc.p(-distance,-distance)))
+	sp_4:runAction(cc.MoveBy:create(moveTime,cc.p(distance,-distance)))
+	self:runAction(transition.sequence{cc.DelayTime:create(moveTime),cc.CallFunc:create(function (obj)
+		obj:removeSelf()
+	end)})
 end
 
 return brick
